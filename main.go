f7ff51d2a9c82b51fa40876cfbc89b8b62cf994b
eduardo.souza <eduardo.souza@hbsis.com.br>
2020-01-17 12:59:12 -0300

    # Added Query String and Path Params example

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -37,12 +39,45 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "not found"}`))
 }
 
+func params(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+
+	userId := -1
+	var err error
+	if val, ok := pathParams["userId"]; ok {
+		userId, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
+
+	commentId := -1
+	if val, ok := pathParams["commentId"]; ok {
+		commentId, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+
+		query := r.URL.Query()
+		location := query.Get("location")
+
+		w.Write([]byte(fmt.Sprintf(`{"userId": %d, "commentId": %d, "location": "%s" }`, userId, commentId, location)))
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", get).Methods(http.MethodGet)
-	r.HandleFunc("/", post).Methods(http.MethodPost)
-	r.HandleFunc("/", put).Methods(http.MethodPut)
-	r.HandleFunc("/", delete).Methods(http.MethodDelete)
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", get).Methods(http.MethodGet)
+	api.HandleFunc("", post).Methods(http.MethodPost)
+	api.HandleFunc("", put).Methods(http.MethodPut)
+	api.HandleFunc("", delete).Methods(http.MethodDelete)
+	api.HandleFunc("/user/{userId}/comment/{commentId}", params).Methods(http.MethodGet)
 	r.HandleFunc("/", notFound)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
